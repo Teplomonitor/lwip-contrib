d6a4d64961be97498b0a0d5ce4384f030404b41d
Andrey Vinogradov <andrey.vinogradov@teplomonitor.ru>
2018-11-20 22:28:46 +0300

    ports/win32/lwipcfg.h: add missing include
    lwip guys rename include file lwipcfg_msvc.h to lwipcfg.h

---
+++ ports/win32/lwipcfg.h
@@ -0,0 +1,15 @@
+/*
+ * lwipcfg.h
+ *
+ *      Author: Andrey
+ */
+
+#ifndef SRC_SHARED_LWIP_SUBMODULE_SRC_INCLUDE_LWIP_LWIPCFG_H_
+#define SRC_SHARED_LWIP_SUBMODULE_SRC_INCLUDE_LWIP_LWIPCFG_H_
+
+
+#ifdef SIMULATION
+#include "lwipcfg_msvc.h"
+#endif
+
+#endif /* SRC_SHARED_LWIP_SUBMODULE_SRC_INCLUDE_LWIP_LWIPCFG_H_ */
